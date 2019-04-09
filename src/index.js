9045e6e8c24360b37f73452e2a3255a62a2d9e65
Christopher Fuentes <fuenteschris75@gmail.com>
2019-04-09 10:14:11 -0600

    Backend code

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const path = require('path');
+const { mongoose }= require('./database');
+
+// Settings
+app.set('port',process.env.PORT || 3000);
+
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+//Routes
+app.use('/api/tasks/',require('./routes/task.routes'))
+
+//Static Files
+app.use(express.static(path.join(__dirname,'public')))
+//Starting server
+app.listen(app.get('port'), ()=>{
+    console.log(`Corriendo en ${app.get('port')}`);
+})
---
+++ src/routes/task.routes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const Task= require('../models/task');
+
+router.get('/', async(req,resp)=>{
+    const tasks = await Task.find();
+    console.log(tasks);
+    resp.json(tasks);
+});
+
+router.get('/:id', async(req,resp)=>{
+    const tasks = await Task.findById(req.params.id);
+    console.log(tasks);
+    resp.json(tasks);
+});
+
+router.post('/', async(req,resp)=>{
+const { title, description } = req.body;
+const task= new Task({title,description});
+await task.save();
+resp.json({status: 'Task Saved'});
+});
+
+router.put('/:id', async(req, resp)=>{
+    const {title,description} = req.body;
+    const newTask= {title,description};
+    await Task.findByIdAndUpdate(req.params.id,newTask);
+    resp.json({status: 'Task Updated'});
+});
+
+router.delete('/:id', async(req,resp)=>{
+await Task.findByIdAndRemove(req.params.id);
+resp.json({status:'Task Deleted'});
+});
+
+
+module.exports= router;
