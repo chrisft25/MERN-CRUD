0b9e9456f97fec609f2687acfa9a2c791562cccf
Christopher Fuentes <fuenteschris75@gmail.com>
2019-04-09 16:46:22 -0600

    Final app

---
+++ src/app/Task.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+
+class Task extends Component{
+    render(){
+        return(
+            <table>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    this.props.tasks.map(task =>{
+                        return(
+                            <tr key={task._id}>
+                                <td>{task.title}</td>
+                                <td>{task.description}</td>
+                                <td>
+                                    <button className="btn light-green darken-4"><i className="material-icons">edit</i></button>
+                                    <button
+                                    className="btn light-red darken-4"
+                                    style={{margin:'4px'}}
+                                    onClick={()=>{
+                                        this.props.deleteTask(task._id);
+                                    }}><i className="material-icons">delete</i></button>
+                                </td>
+
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+        </table>
+        )
+    }
+}
+
+export default Task;
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,5 @@
-alert('React goes here');
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import App from './App';
+
+render(<App/>,document.getElementById('app'));
