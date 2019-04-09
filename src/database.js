b305884dd4cde00b70fce4f2eade3a84575c4a0b
Christopher Fuentes <fuenteschris75@gmail.com>
2019-04-09 17:31:45 -0600

    Final Project

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const URI= 'mongodb://localhost/mern-tasks';
 
-mongoose.connect(URI)
+mongoose.connect(URI,{useNewUrlParser: true})
     .then(db=> console.log('DB is connected'))
     .catch(err=> console.log(err));
 
